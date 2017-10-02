db75bc175d1abb3c358fe4a497c14d2e8ce5a615
stanlee321 <stanlee321@gmail.com>
2017-10-01 23:29:57 -0400

    Little fix

--- mainmulti.py
+++ mainmulti.py
@@ -474,8 +474,8 @@ if __name__ == '__main__':
 	semaforo = CreateSemaforo(periodoSemaforo = 10)
 	poligono  = data[0]
 	src = ['./installationFiles/mySquare.mp4', 0]
-	vs = WebcamVideoStream(src=src[0], height = 640, width = 480).start()
-	#vs = WebcamVideoStream(src=src[1], height = 2048, width = 1536, queueSize=128).start()
+	#vs = WebcamVideoStream(src=src[1], height = 640, width = 480).start()
+	vs = WebcamVideoStream(src=src[1], height = 2048, width = 1536, queueSize=128).start()
 	#vs = WebcamVideoStream(src=src[1], height = 2592, width = 1944, queueSize=128).start()
 	#vs = WebcamVideoStream(src=src[1], height = 3266, width = 2450, queueSize=128).start()
 	time.sleep(1.0)
