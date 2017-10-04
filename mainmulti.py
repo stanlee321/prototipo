a15392142b58f51191db521ed37130e282950ebc
stanlee321 <stanlee321@gmail.com>
2017-10-04 03:46:32 -0400

    FPS tests

--- mainmulti.py
+++ mainmulti.py
@@ -95,10 +95,9 @@ if __name__ == '__main__':
 	poligono  = data[0]
 	src = ['./installationFiles/mySquare.mp4', 0]
 	#vs = WebcamVideoStream(src=src[0], height = 640, width = 480).start()
-	#vs = WebcamVideoStream(src=src[1], height = 2048, width = 1536, queueSize=8).start()
-	#vs = WebcamVideoStream(src=src[1], height = 2048, width = 1536).start()
+	vs = WebcamVideoStream(src=src[1], height = 2048, width = 1536).start()
 	#vs = WebcamVideoStream(src=src[1], height = 2592, width = 1944).start()
-	vs = WebcamVideoStream(src=src[1], height = 3266, width = 2450).start()
+	#vs = WebcamVideoStream(src=src[1], height = 3266, width = 2450).start()
 	
 	time.sleep(1.0)
 	fps = FPS().start() 
