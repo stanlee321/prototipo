5d2ec152da3893b3325b5bc5a227554025dbcd98
stanlee321 <stanlee321@gmail.com>
2017-10-04 01:12:22 -0400

    added 8 mpx tests

--- mainmulti.py
+++ mainmulti.py
@@ -85,11 +85,11 @@ if __name__ == '__main__':
 	semaforo = CreateSemaforo(periodoSemaforo = 10)
 	poligono  = data[0]
 	src = ['./installationFiles/mySquare.mp4', 0]
-	vs = WebcamVideoStream(src=src[0], height = 640, width = 480).start()
+	#vs = WebcamVideoStream(src=src[0], height = 640, width = 480).start()
 	#vs = WebcamVideoStream(src=src[1], height = 2048, width = 1536, queueSize=8).start()
 	#vs = WebcamVideoStream(src=src[1], height = 2048, width = 1536).start()
 	#vs = WebcamVideoStream(src=src[1], height = 2592, width = 1944, queueSize=128).start()
-	#vs = WebcamVideoStream(src=src[1], height = 3266, width = 2450, queueSize=128).start()
+	vs = WebcamVideoStream(src=src[0], height = 3266, width = 2450, queueSize=128).start()
 	
 	time.sleep(1.0)
 	fps = FPS().start() 
@@ -105,7 +105,6 @@ if __name__ == '__main__':
 
 	#function1 = Function_1() # Saver to db
 	#function2 = Function_2() # BG substractor
-
 	#bgsub_CNT = CreateBGCNT()
 
 	#pipeline = PipelineRunner(pipeline=[MultiJobs( fun1 = function1, fun2 = function2)], log_level=logging.DEBUG)
