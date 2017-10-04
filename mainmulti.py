3e426a3dab3e18a1c2e9a473751913361e15ad5a
stanlee321 <stanlee321@gmail.com>
2017-10-04 01:30:21 -0400

    fixes

--- mainmulti.py
+++ mainmulti.py
@@ -90,8 +90,8 @@ if __name__ == '__main__':
 	#vs = WebcamVideoStream(src=src[0], height = 640, width = 480).start()
 	#vs = WebcamVideoStream(src=src[1], height = 2048, width = 1536, queueSize=8).start()
 	#vs = WebcamVideoStream(src=src[1], height = 2048, width = 1536).start()
-	#vs = WebcamVideoStream(src=src[1], height = 2592, width = 1944, queueSize=128).start()
-	vs = WebcamVideoStream(src=src[0], height = 3266, width = 2450, queueSize=128).start()
+	#vs = WebcamVideoStream(src=src[1], height = 2592, width = 1944).start()
+	vs = WebcamVideoStream(src=src[0], height = 3266, width = 2450).start()
 	
 	time.sleep(1.0)
 	fps = FPS().start() 
