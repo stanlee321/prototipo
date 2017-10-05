856ffca4dcc4a0e01211721c3b86fd4d0fadbaba
stanlee321 <stanlee321@gmail.com>
2017-10-05 18:09:16 -0400

    fixes

--- mainmulti.py
+++ mainmulti.py
@@ -145,7 +145,8 @@ if __name__ == '__main__':
 		# to have a maximum width of 400 pixels
 		t1 = time.time()
 		frame = vs.read()
-		frame, frame_resized = process_resize(frame)
+		frame = imutils.resize(frame)
+		frame_resized = imutils.resize(frame, (320,240))
 		print('frame shape', frame.shape)
 		print('frame_resized', frame_resized.shape)
 
