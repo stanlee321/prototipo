5e897b1068424bd50fefe009eb4480332d911332
stanlee321 <stanlee321@gmail.com>
2017-10-05 16:50:41 -0400

    picam

--- camPi.py
+++ camPi.py
@@ -17,6 +17,7 @@ class Video_Camera(Thread):
         self.mins = mins
         self.camera = pc.PiCamera( resolution=(width,height), framerate=int(self.fps))
         self.stream = 0
+        self.frame = 0
         #...
          
     def initialize_camera(self):
