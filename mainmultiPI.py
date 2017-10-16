4eb1f3f9538ea025479de9a2b9d249413772357d
stanlee321 <stanlee321@gmail.com>
2017-10-16 17:08:41 -0400

    fixes

--- mainmultiPI.py
+++ mainmultiPI.py
@@ -134,7 +134,7 @@ def create_main(src):
 		print('alll the while took', t2-t1)
 		# update the FPS counter
 		
-		cv2.imshow('frame', frames)
+		cv2.imshow('frame', frame)
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
 
