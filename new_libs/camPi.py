0e52dbe8f9515743adaa891f6d7c1840a990411d
stanlee321 <stanlee321@gmail.com>
2017-10-16 18:17:05 -0400

    campi updates

--- new_libs/camPi.py
+++ new_libs/camPi.py
@@ -1,28 +1,15 @@
-# camPi2
-
-# thanks to Adrian and his page pyimagesearch and his post about increasing
-# the speed of fps in rapsberry pi
-
 # import the necessary packages
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 from threading import Thread
 import cv2
-import time
-
+ 
 class PiVideoStream:
-    def __init__(self, resolution=(320, 240), framerate=32, vf=False, hf=False):
+    def __init__(self, resolution=(320, 240), framerate=32):
         # initialize the camera and stream
         self.camera = PiCamera()
         self.camera.resolution = resolution
         self.camera.framerate = framerate
-        self.camera.vflip = vf
-        self.camera.hflip = hf
-
-        #self.camera.exposure_mode = 'off'
-
-        #time.sleep(1)
-
         self.rawCapture = PiRGBArray(self.camera, size=resolution)
         self.stream = self.camera.capture_continuous(self.rawCapture,
             format="bgr", use_video_port=True)
@@ -31,10 +18,6 @@ class PiVideoStream:
         # if the thread should be stopped
         self.frame = None
         self.stopped = False
-
-
-        self.frame_resized = None
-        
     def start(self):
         # start the thread to read frames from the video stream
         Thread(target=self.update, args=()).start()
@@ -46,9 +29,6 @@ class PiVideoStream:
             # grab the frame from the stream and clear the stream in
             # preparation for the next frame
             self.frame = f.array
-            #self.frame_resized = cv2.resize(self.frame, (320,240))
-            #self.frame_resized = cv2.resize(self.frame, (160,120))
-
             self.rawCapture.truncate(0)
  
             # if the thread indicator variable is set, stop the thread
@@ -61,7 +41,7 @@ class PiVideoStream:
 
     def read(self):
         # return the frame most recently read
-        return self.frame#, self.frame_resized
+        return self.frame
  
     def stop(self):
         # indicate that the thread should be stopped
