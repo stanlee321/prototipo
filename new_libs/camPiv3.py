a8a272844f3a82074601f897fb14c48388fa986a
stanlee321 <stanlee321@gmail.com>
2017-10-17 15:48:33 -0400

    fixed error of macrolimit in campiv3

--- new_libs/camPiv3.py
+++ new_libs/camPiv3.py
@@ -4,8 +4,8 @@ import picamera
 
 with picamera.PiCamera() as camera:
     camera.resolution = (3264, 2448)
-    camera.framerate = 30
-    camera.start_recording('highres.h264')
+    camera.framerate = 12
+    camera.start_recording('highres.mjpg')
     camera.start_recording('lowres.h264', splitter_port=2, resize=(320, 240))
     camera.wait_recording(30)
     camera.stop_recording(splitter_port=2)
