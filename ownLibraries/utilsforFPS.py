f251c9b129f77d4739636f739d0cf5f8fb1dd300
stanlee321 <stanlee321@gmail.com>
2017-10-18 23:54:45 -0400

    added changes to videostream.py lib for w,h corrections

---
+++ ownLibraries/utilsforFPS.py
@@ -0,0 +1,80 @@
+# From http://www.pyimagesearch.com/2015/12/21/increasing-webcam-fps-with-python-and-opencv/
+
+import cv2
+import datetime
+from threading import Thread
+
+
+class FPS:
+    def __init__(self):
+        # store the start time, end time, and total number of frames
+        # that were examined between the start and end intervals
+        self._start = None
+        self._end = None
+        self._numFrames = 0
+
+    def start(self):
+        # start the timer
+        self._start = datetime.datetime.now()
+        return self
+
+    def stop(self):
+        # stop the timer
+        self._end = datetime.datetime.now()
+
+    def update(self):
+        # increment the total number of frames examined during the
+        # start and end intervals
+        self._numFrames += 1
+
+    def elapsed(self):
+        # return the total number of seconds between the start and
+        # end interval
+        return (self._end - self._start).total_seconds()
+
+    def fps(self):
+        # compute the (approximate) frames per second
+        return self._numFrames / self.elapsed()
+
+
+class WebcamVideoStream:
+    def __init__(self, src, width, height):
+        # initialize the video camera stream and read the first frame
+        # from the stream
+        self.stream = cv2.VideoCapture(src)
+        self.stream.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+        (self.grabbed, self.frame) = self.stream.read()
+
+        
+        self.frame_resized = cv2.resize(self.frame,(320,240), fx = 1.2, fy = 1.2)
+        # initialize the variable used to indicate if the thread should
+        # be stopped
+        self.stopped = False
+
+    def start(self):
+        # start the thread to read frames from the video stream
+        Thread(target=self.update, args=()).start()
+        return self
+
+    def update(self):
+        # keep looping infinitely until the thread is stopped
+        while True:
+            # if the thread indicator variable is set, stop the thread
+            if self.stopped:
+                return
+
+            # otherwise, read the next frame from the stream
+            (self.grabbed, self.frame) = self.stream.read()
+            #self.frame_resized = cv2.resize(self.frame,(320,240))
+            self.frame_resized = cv2.resize(self.frame,(320,240), fx = 1.2, fy = 1.2)
+
+    def read(self):
+        # return the frame most recently read
+
+        return self.frame, self.frame_resized
+
+    def stop(self):
+        # indicate that the thread should be stopped
+        self.stopped = True
+
--- ownLibraries/videostreamerlib.py
+++ ownLibraries/videostreamerlib.py
@@ -5,7 +5,6 @@ import cv2
 import datetime
 
 
-
 class FPS:
     def __init__(self):
         # store the start time, end time, and total number of frames
@@ -41,22 +40,20 @@ class FPS:
 class WebcamVideoStream:
 	def __init__(self, src=0, resolution = (320,240)):
 
-		width = resolution[0]
-		height = resolution[1]
+		width, height= resolution[0], resolution[1]
 		# initialize the video camera stream and read the first frame
 		# from the stream
 		self.stream = cv2.VideoCapture(src)
 		self.stream.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 		self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 		(self.grabbed, self.frame) = self.stream.read()
-
 		# initialize the variable used to indicate if the thread should
 		# be stopped
 		self.stopped = False
 
 		# Resized artifact variable
 		#self.frame_resized = cv2.resize(self.frame, (320,240))	
-		self.frame_resized = None
+		self.frame_resized =  cv2.resize(self.frame, (320,240))
 	def start(self):
 		# start the thread to read frames from the video stream
 		t = Thread(target=self.update, args=())
@@ -76,7 +73,7 @@ class WebcamVideoStream:
 
 			# Set new resolution for the consumers
 			self.frame_resized = cv2.resize(self.frame, (320,240))
-
+			#print('shape?',self.frame_resized.shape)
 
 	def read(self):
 		# return the frame most recently read
