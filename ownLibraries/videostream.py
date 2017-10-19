f269880e53548ee551419147874c5f9fdb3b9b05
stanlee321 <stanlee321@gmail.com>
2017-10-19 00:32:42 -0400

    merged all FPS and WebvideoStream in videostream.py and added staticmethods in BackGroundSubCNT for get_centroid

--- ownLibraries/videostream.py
+++ ownLibraries/videostream.py
@@ -1,7 +1,88 @@
 
 # import the necessary packages
-from videostreamerlib import WebcamVideoStream
-from videostreamerlib import FPS
+from threading import Thread
+import cv2
+import datetime
+
+
+class FPS:
+    def __init__(self):
+        # store the start time, end time, and total number of frames
+        # that were examined between the start and end intervals
+        self._start = None
+        self._end = None
+        self._numFrames = 0
+
+    def start(self):
+        # start the timer
+        self._start = datetime.datetime.now()
+        return self
+
+    def stop(self):
+        # stop the timer
+        self._end = datetime.datetime.now()
+
+    def update(self):
+        # increment the total number of frames examined during the
+        # start and end intervals
+        self._numFrames += 1
+
+    def elapsed(self):
+        # return the total number of seconds between the start and
+        # end interval
+        return (self._end - self._start).total_seconds()
+
+    def fps(self):
+        # compute the (approximate) frames per second
+        return self._numFrames / self.elapsed()
+
+
+class WebcamVideoStream:
+	def __init__(self, src=0, resolution = (320,240)):
+
+		width, height= resolution[0], resolution[1]
+		# initialize the video camera stream and read the first frame
+		# from the stream
+		self.stream = cv2.VideoCapture(src)
+		self.stream.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+		self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+		(self.grabbed, self.frame) = self.stream.read()
+		# initialize the variable used to indicate if the thread should
+		# be stopped
+		self.stopped = False
+
+		# Resized artifact variable
+		#self.frame_resized = cv2.resize(self.frame, (320,240))	
+		self.frame_resized =  cv2.resize(self.frame, (320,240))
+	def start(self):
+		# start the thread to read frames from the video stream
+		t = Thread(target=self.update, args=())
+		t.daemon = True
+		t.start()
+		return self
+
+	def update(self):
+		# keep looping infinitely until the thread is stopped
+		while True:
+			# if the thread indicator variable is set, stop the thread
+			if self.stopped:
+				return
+
+			# otherwise, read the next frame from the stream
+			(self.grabbed, self.frame) = self.stream.read()
+
+			# Set new resolution for the consumers
+			self.frame_resized = cv2.resize(self.frame, (320,240))
+			#print('shape?',self.frame_resized.shape)
+
+	def read(self):
+		# return the frame most recently read
+		return self.frame, self.frame_resized
+
+	def stop(self):
+		# indicate that the thread should be stopped
+		self.stopped = True
+
 
 
 class VideoStream:
