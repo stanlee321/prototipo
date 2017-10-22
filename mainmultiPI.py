813397b9f95ed35eb94ac196553632197469cbef
stanlee321 <stanlee321@gmail.com>
2017-10-22 16:49:03 -0400

    removed frame_number break limit

--- mainmultiPI.py
+++ mainmultiPI.py
@@ -43,8 +43,8 @@ ENCODING = 'utf-8'
 def create_main(fnt):
 
 	data = np.load('./installationFiles/mySquare.npy')
-	#fuente = ['./installationFiles/mySquare.mp4', 0]
-	fuente = ['../trialVideos/mySquare.mp4', 0]
+	fuente = ['./installationFiles/mySquare.mp4', 0]
+	#fuente = ['../trialVideos/mySquare.mp4', 0]
 	print(data)
 	semaforo = CreateSemaforo(periodoSemaforo = 0)
 	poligono  = data[0]
@@ -110,11 +110,11 @@ def create_main(fnt):
 		print('alll the while took', t6-t5)
 
 	
-		cv2.imshow('frame', cv2.resize(frame_resized,(320,240)))
+		cv2.imshow('frame', frame_resized)
 		#cv2.imwrite('frame.jpg', cv2.resize(frame_resized,(640,480)))
 		#break
-		if _frame_number == 2500:
-			break
+		#if _frame_number == 2500:
+		#	break
 
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
