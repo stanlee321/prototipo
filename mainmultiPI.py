5f622fc8f640cb9ca30689b229b6e1725399c1fe
stanlee321 <stanlee321@gmail.com>
2017-10-22 16:16:44 -0400

    added source outside of root for videosource

--- mainmultiPI.py
+++ mainmultiPI.py
@@ -110,7 +110,7 @@ def create_main(fnt):
 		print('alll the while took', t6-t5)
 
 	
-		cv2.imshow('frame', cv2.resize(frame_resized,(320,240)))
+		#cv2.imshow('frame', cv2.resize(frame_resized,(320,240)))
 		#cv2.imwrite('frame.jpg', cv2.resize(frame_resized,(640,480)))
 		#break
 		if _frame_number == 2500:
