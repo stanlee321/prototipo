4db8f9f89b704ea6757a6cfdca8c34b149c31750
stanlee321 <stanlee321@gmail.com>
2017-10-22 19:04:22 -0400

    test in 8 mpx in rpi

--- mainmultiPI.py
+++ mainmultiPI.py
@@ -42,19 +42,23 @@ ENCODING = 'utf-8'
 
 def create_main(fnt):
 
-	data = np.load('./installationFiles/mySquare.npy')
-	#fuente = ['./installationFiles/mySquare.mp4', 0]
-	fuente = ['../trialVideos/mySquare.mp4', 0]
+	data = np.load('./installationFiles/heroes.npy')
+	fuente = ['./installationFiles/heroes.mp4', 0]
+	#fuente = ['../trialVideos/mySquare.mp4', 0]
 	print(data)
 	semaforo = CreateSemaforo(periodoSemaforo = 0)
 	poligono  = data[0]
 
 	ON = True
 
-	height = 640
-	width = 480
+	#height = 640
+	#width = 480
+
+	height = 3264
+	width = 2448
 
 	#bg = CreateBGCNT()
+	vs = VideoStream(src = fuente[fnt], resolution = (height, width), poligono = poligono, draw=True).start() # 0.5 pmx
 	vs = VideoStream(src = fuente[fnt], resolution = (height, width), poligono = poligono, draw=True).start() # 0.5 pmx
 	#vs = WebcamVideoStream(src=src[1], height = 2048, width = 1536).start()	# 2 mpx
 	#vs = WebcamVideoStream(src=src[1], height = 2560, width = 1920).start()	# 5 mpx
@@ -113,7 +117,7 @@ def create_main(fnt):
 		#cv2.imshow('frame',cv2.resize(frame_resized,(640,480)))
 		#cv2.imwrite('../frames/frame_{}.jpg'.format(frame_number), cv2.resize(frame_resized,(640,480)))
 		#break
-		if _frame_number == 2500:
+		if _frame_number == 100:
 			break
 
 		if cv2.waitKey(1) & 0xFF == ord('q'):
