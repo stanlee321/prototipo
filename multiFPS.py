05e8a750d42240e6eb02d93939c008251c467a26
stanlee321 <stanlee321@gmail.com>
2017-10-31 13:15:22 -0400

    framereate decreased... pi not black?

--- multiFPS.py
+++ multiFPS.py
@@ -45,14 +45,13 @@ print("[INFO] sampling THREADED frames from webcam...")
 #height = 2464
 
 
+#5mp
+width=2592
+height=1944
 
-# 5mp
-#width=2592
-#height=1944
 # 4mp
-
-width = 2240
-height = 1680
+#width = 2240
+#height = 1680
 
 # 3mp
 #width = 2048
