28b42a898cbcac3c8161bc9e0a28a780879cb493
stanlee321 <stanlee321@gmail.com>
2017-10-31 12:50:05 -0400

    added 3 mpx res

--- multiFPS.py
+++ multiFPS.py
@@ -49,12 +49,12 @@ print("[INFO] sampling THREADED frames from webcam...")
 #height=1944
 
 # 3mp
-#width = 2048
-#height = 1536
+width = 2048
+height = 1536
 
 # 2mp
-width =1600
-height = 1200 
+#width =1600
+#height = 1200 
 
 # 1mp
 #width = 1280
