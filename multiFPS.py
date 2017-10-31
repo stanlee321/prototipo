77f799b22c091b76b825196a0edf34a745dfb09e
stanlee321 <stanlee321@gmail.com>
2017-10-31 12:20:59 -0400

    added interfase for picam and classic vdeocapture

--- multiFPS.py
+++ multiFPS.py
@@ -20,19 +20,19 @@ directorioDeLibreriasPropias = directorioDeTrabajo +'/ownLibraries'
 saveDirectory = directorioDeTrabajo+'/VideoCapture/'
 
 sys.path.insert(0, directorioDeLibreriasPropias)
-from ownLibraries.webcamvideostream import WebcamVideoStream
+
+from ownLibraries.videoStreamTest import WebcamVideoStream
 from ownLibraries.videostreamv5 import FPS
 
 if not os.path.exists(saveDirectory):
 	os.makedirs(saveDirectory)
 
 date_string = datetime.datetime.now().strftime('%Y-%m-%d-%H:%M')
+
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-n", "--num-frames", type=int, default=100,
-    help="# of frames to loop over for FPS test")
-ap.add_argument("-d", "--display", type=int, default=1,
-    help="Whether or not frames should be displayed")
+ap.add_argument("-p", "--picamera", type=int, default=-1,
+	help="whether or not the Raspberry Pi camera should be used")
 args = vars(ap.parse_args())
 
 # created a *threaded* video stream, allow the camera sensor to warmup,
@@ -74,7 +74,9 @@ xMax = int(4/5*width)
 yMin = int(1/5*height)
 yMax = int(4/5*height)
 #vs = WebcamVideoStream(src=0,width=2592, height=1944).start()
-vs = WebcamVideoStream(src=0, resolution=(width,height)).start()
+
+vs = VideoStream(usePiCamera=args["picamera"] > 0, resolution=(width,height)).start()
+#vs = WebcamVideoStream(src=0, resolution=(width,height)).start()
 fps = FPS().start()
  
 # loop over some frames...this time using the threaded stream
