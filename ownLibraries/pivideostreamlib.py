2acec35e47d1341a1f0f077b4ff1e2b24f1c53a5
stanlee321 <stanlee321@gmail.com>
2017-10-31 12:34:30 -0400

    added frame, frame_low as outputs before was like typple

--- ownLibraries/pivideostreamlib.py
+++ ownLibraries/pivideostreamlib.py
@@ -31,7 +31,7 @@ class PiVideoStream:
         self.stopped = False
 
 
-        #self.frame_resized =  cv2.resize(self.frame, (320,240))
+        self.frame_resized =  None
 
         
     def start(self):
