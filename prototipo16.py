b3fede5e33f253d1b352258e284db7a06c354e38
stanlee321 <stanlee321@gmail.com>
2017-11-05 02:11:31 -0400

    added fixes

--- prototipo16.py
+++ prototipo16.py
@@ -174,11 +174,13 @@ def __main_function__():
 		
 		else:
 			pass
-			#print('[passs]', informacion['semaforo'][3])
+		
 		# Si tengo infracciones pendientes las evoluciono
 
+		if informacion['semaforo'][0] == 2:
+			print('Yellow Yellow...')
 
-		if informacion['semaforo'][0] >= 1:							# Si estamos en rojo, realizamos una accion
+		elif informacion['semaforo'][0] < 2:							# Si estamos en rojo, realizamos una accion
 			if informacion['semaforo'][2] == 1:						# esto se inicia al principio de este estado
 				miReporte.info('SEMAFORO EN ROJO')
 				miPoliciaReportando.inicializarAgente()
@@ -187,7 +189,8 @@ def __main_function__():
 				frame_number = 0
 
 			cambiosImportantes = miPoliciaReportando.seguirObjeto(frame_number,informacion)
-
+		else:
+			pass
 		if informacion['semaforo'][0] == 0:							# Si estamos en verde realizamos otra accion
 			if informacion['semaforo'][2] == -1:					# Si estamos en verde y en flanco, primer verde, realizamos algo
 				miReporte.info('Infracciones: '+str(miPoliciaReportando.numeroInfraccionesConfirmadas()))
