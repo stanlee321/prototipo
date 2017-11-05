a86d8d808dac6fcdda450518c4e358207b96b743
stanlee321 <stanlee321@gmail.com>
2017-11-05 02:38:05 -0400

    fxies

--- prototipo16.py
+++ prototipo16.py
@@ -177,7 +177,7 @@ def __main_function__():
 		
 		# Si tengo infracciones pendientes las evoluciono
 		if informacion['semaforo'][0] == 1 :							# Si estamos en rojo, realizamos una accion
-			print('estoy dentro')
+			print('estoy dentro, pero... informacion[semaforo][2]', informacion['semaforo'][2], 'no es == 1')
 			if informacion['semaforo'][2] == 1:							# esto se inicia al principio de este estado
 				print('Entonces Fue...', informacion['semaforo'][2])
 				miReporte.info('SEMAFORO EN ROJO')
