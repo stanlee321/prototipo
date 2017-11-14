68e9c3ece48926aa27a0b972f7a1a7735d4a4ac8
Alvaro Hurtado <a.hurtado.bo@gmail.com>
2017-11-14 12:31:51 -0400

    changed source of picamera to 1

--- ownLibraries/policiainfractor.py
+++ ownLibraries/policiainfractor.py
@@ -57,7 +57,7 @@ class PoliciaInfractor():
 		self.ultimaVelocidad = 0
 		self.segundaCamara = segundaCamara
 		eightMP = (3296,2512)
-		piCamSource  = 0
+		piCamSource  = 1
 		if self.segundaCamara:
 			self.camaraAlta = Shooter(video_source = piCamSource, width = eightMP[0], height = eightMP[1])
 
