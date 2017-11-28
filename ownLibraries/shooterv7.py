639e6d4a949a742796ce0477fd8bb1a9693ee7c5
stanlee321 <stanlee321@gmail.com>
2017-11-28 10:28:53 -0400

    added shooterv6 to initial conditions, createed v7 for multiprofeactures

--- ownLibraries/shooterv7.py
+++ ownLibraries/shooterv7.py
@@ -51,6 +51,14 @@ class Shooter():
 		self.camera.resolution = (self.width,self.height)
 		self.camera.framerate = 1
 		self.camera.start_preview()
+
+		self.input_q = multiprocessing.Queue(maxsize = 5)
+
+		process = multiprocessing.Process(target = self.start, args=((self.input_q,)))
+		process.daemon = True
+		pool = multiprocessing.Pool(4, self.start, (self.input_q,))
+
+
 		print('EXITOSAMENTE CREE LA CLASE SHOOTER')
 
 
@@ -65,8 +73,8 @@ class Shooter():
 		self.saveDir = self.directorioDeGuardadoGeneral +"/" + folder
 		if not os.path.exists(self.saveDir):
 			os.makedirs(self.saveDir) 
-		self.eyesOpen = True
-		#self.start()
+		#self.eyesOpen = True
+		self.feed(self.input_q)
 		print('Encendi Camara de Forma Exitosa en ' + self.saveDir)
 
 	def encenderCamara(self):
@@ -78,25 +86,29 @@ class Shooter():
 		print('Realizada las capturas, cerrando ojos...')
 	
 
-	def writter(self):
+	def writter(self, data):
 		#while not input_queue.empty:
-		self.frame_number = 0
-		while self.frame_number < self.maxCapturas:
-			print('GUARDADO en: '+ self.saveDir+'/{}-{}.jpg'.format(self.fechaInfraccion[:-3], self.frame_number))
+		saveDir, fechaInfraccion, maxCapturas, frame_number = data[0], data[1], data[2]
+		frame_number = 0
+		while frame_number < maxCapturas:
+			print('GUARDADO en: '+ saveDir+'/{}-{}.jpg'.format(self.fechaInfraccion[:-3], frame_number))
 			#yield "image%02d.jpg" % frame
-			
-			yield self.saveDir+"/{}-{}.jpg".format(self.fechaInfraccion, self.frame_number)
+			yield saveDir+"/{}-{}.jpg".format(fechaInfraccion, frame_number)
 			#yield "./imagen_{}.jpg".format(self.frame_number)
-			self.frame_number += 1
-
-
-
-	def start(self):
-		start = time.time()
-		self.camera.capture_sequence(self.writter(), use_video_port=True)
-		finish = time.time()
-		self.eyesOpen = False
-		print("Captured %d frames at %.2ffps" % (self.maxCapturas,self.maxCapturas / (finish - start)))
+			frame_number += 1
+	def start(self, input_q):
+		while True:
+			data = input_q.get()
+			camera = data[5]
+			writter = data[6]
+			start = time.time()
+			camera.capture_sequence(writter(data), use_video_port=True)
+			finish = time.time()
+			self.eyesOpen = False
+			print("Captured %d frames at %.2ffps" % (data[2],data[2] / (finish - start)))
+	def feed(self, input_q):
+		input_q.put([self.saveDir, self.fechaInfraccion, self.maxCapturas, self.frame_number, self.camera,self.writter])
+		return self
 
 if __name__ == '__main__':
 	#DEMO DEMO DEMO 
