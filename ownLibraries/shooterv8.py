4c2504b063becae1688db34fe068311ec448f60e
stanlee321 <stanlee321@gmail.com>
2017-12-08 11:49:20 -0400

    working in 5 mpxs

--- ownLibraries/shooterv8.py
+++ ownLibraries/shooterv8.py
@@ -22,8 +22,8 @@ class Shooter():
 	directorioDeNumpy = os.getenv('HOME')+'/trafficFlow/prototipo/installationFiles/'
 	date_hour_string = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S:%f')
 
-	def __init__(self, video_source = 0, width = 3280, height = 2464, cutPoly=([10,10],[3280,2464]), capturas = 5):
-	#def __init__(self, video_source = 0, width = 2592, height = 1944, cutPoly=([10,10],[2592,1944]), capturas = 2):
+	#def __init__(self, video_source = 0, width = 3280, height = 2464, cutPoly=([10,10],[3280,2464]), capturas = 5):
+	def __init__(self, video_source = 0, width = 2592, height = 1944, cutPoly=([10,10],[2592,1944]), capturas = 2):
 		
 		data = np.load(Shooter.directorioDeNumpy+'datos.npy')
 		# Initial aparemeters
