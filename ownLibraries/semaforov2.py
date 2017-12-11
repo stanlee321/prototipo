2e5c5558b53f0f4a07e163170e2ff5ed3d516f6c
stanlee321 <stanlee321@gmail.com>
2017-12-11 11:31:33 -0400

    added rthrs

--- ownLibraries/semaforov2.py
+++ ownLibraries/semaforov2.py
@@ -199,7 +199,7 @@ class Real(Semaforo):
 		self.upper_yellow = np.array([27,255,255], dtype=np.uint8)
 
 		# RED range
-		self.lower_red = np.array([0,30,0], dtype=np.uint8) #_,100,_ # 140,70,_
+		self.lower_red = np.array([10,150,0], dtype=np.uint8) #_,100,_ # 140,70,_
 		self.upper_red = np.array([180,255,255], dtype=np.uint8)
 
 		# GREEN range
