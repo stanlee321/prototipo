3fc29583cc29ef31db1f8d95ef6d9d9a407ee979
stanlee321 <stanlee321@gmail.com>
2017-12-11 12:04:03 -0400

    addeds as

--- ownLibraries/semaforov2.py
+++ ownLibraries/semaforov2.py
@@ -199,7 +199,7 @@ class Real(Semaforo):
 		self.upper_yellow = np.array([27,255,255], dtype=np.uint8)
 
 		# RED range
-		self.lower_red = np.array([0,140,0], dtype=np.uint8) #_,100,_ # 140,70,_
+		self.lower_red = np.array([0,160,0], dtype=np.uint8) #_,100,_ # 140,70,_
 		self.upper_red = np.array([180,255,255], dtype=np.uint8)
 
 		# GREEN range
