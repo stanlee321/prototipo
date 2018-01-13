5d8dc519bec335767b9f817879e7017ba185e9a8
stanlee321 <stanlee321@gmail.com>
2018-01-13 19:28:33 -0400

    adde debugs for shooterv10

--- ownLibraries/shooterv10.py
+++ ownLibraries/shooterv10.py
@@ -162,11 +162,7 @@ class Shooter():
 				observador.move_captures(index_real)
 				watermarker.put_watermark(saveDir, timestamp)
 				#delete old files without watermwark
-				for old_image in paths.list_images(saveDir):
-					try:
-						os.remove(old_image)
-					except:
-						print('this file does not exist:', old_image)
+
 
 				
 			else:
