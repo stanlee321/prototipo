aad240f628812b8a769bbd3b9c530f1d50814f90
stanlee321 <stanlee321@gmail.com>
2018-01-13 17:32:42 -0400

    adde debugs for shooterv10

--- ownLibraries/shooterv10.py
+++ ownLibraries/shooterv10.py
@@ -31,7 +31,7 @@ class Shooter():
 	date_hour_string = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S:%f')
 	path_to_logo = 	os.getenv('HOME')+'/'+ 'trafficFlow' +'/' +'prototipo/'+ 'watermark'+ '/dems.png'
 
-	def __init__(self, video_source = 0, width = 3280, height = 2464, cutPoly=([10,10],[3280,2464]), capturas = 3):
+	def __init__(self, video_source = 0, width = 3280, height = 2464, cutPoly=([10,10],[3280,2464]), capturas = 1):
 	#def __init__(self, video_source = 0, width = 2592, height = 1944, cutPoly=([10,10],[2592,1944]), capturas = 5):
 		
 		data = np.load(Shooter.directorioDeNumpy+'datos.npy')
