5481938d2201d3c811f0c779088469688d88bc06
stanlee321 <stanlee321@gmail.com>
2018-02-17 23:22:57 -0400

    updated README file

--- README.md
+++ README.md
@@ -1,34 +1,51 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-# multiprocessing project
-Implemationtmultipro in the picam.
+# Smarth-Embeded-Camera and Traffic Light infractor
+Implemation of a Smart Embedded Camera, this repo contains the necesary code to 
+implement a Traffic Light infraction detector using a Raspberry Pi, Cameras and 
+computer vision  and machine learning techniques.
 
-In Bolivia there is a huge problem regarding Road culture, this project intends to solve this problem with the use of Artificial Inteliligence and Internet of things devices
+In Bolivia there is a huge problem regarding Road culture, this project intends to solve this problem with the use of Artificial Inteliligence and Internet of things devices.
 
-In this repository we develop a method for getting a high resolution video with high frames per second. Focusing on the interest features like Licence Plates and deciding if a Traffic light is in Red Light and if a Car happens to go cross the red light breaking the current laws
+## SmartCamera project
+
+Implementation SmartCamera with a picam, python, opencv and machine learning techniques.
 
 
-=======
-# prototipo
-In this repository we synchronice the last version of our Automatic Infractor
->>>>>>> c9bb481f84d8250c30c39d911e0340ad76159c55
-=======
-# SmartCamera project
-
-Implementation SmartCamera with a picam using python, opencv and machine learning techniques.
-
-You can see our results in our page [here](http://deepmicrosystems.com/visual/)
+You can see our results in our page [here](http://deepmicrosystems.com/)
 
 And a video demo also 
 [[![Video visualization demo](https://i.ytimg.com/vi/4Ew56N_uMIk/2.jpg)](https://youtu.be/4Ew56N_uMIk)
 
 
-In Bolivia there is a huge problem regarding Road culture, this project intends to solve this problem with the use of Artificial Inteliligence and Internet of things devices to create a SmartCamera running in a Pi3 device, this repo contaion the basic code for his implementation
+## Getting Started
+1. `pip3 install -r requirements.txt`
+2. `python3 prototipo25.py Show` 
+    Optional arguments (default value a folder with todays date):
+    * Folder to workon `Show'`
+    * Test in custom video `myVideo.mp4'`
 
-In this repo we develop a method for getting a high resolution video with high frames per second. Focusing on the interest features like Licence Plates and deciding if a Traffic light is in Red Light and if a Car happens to go cross the red light breaking the current laws
+## Requirements
+### Hardware 
+- Your RPi3 must have conected to
+- 5 or 8 MP NoIR PiCamera ( for take the HD shoots )
+- Simple USB WebCam (camera for detec the flow of cars)
+- Optional ( embeded circuit to control the IR filter)
+
+### Software
+- [Python 3.5](https://www.continuum.io/downloads)
+- [OpenCV 3.X](http://opencv.org/) (just for write the plates in the image )
+- [scipy] (for write the images into disk)
+- [scikit-learn] (for run the trafficlight color detector sensor)
+- [sqlite3] (for track information about the Shutter PiCamera class)
 
 
-=======
-# Prototipo
-In this repository we synchronice the last version of our Automatic Infractor. In the last commit we are developing a multiprocessing feature for increase the speed of the process.
->>>>>>> 2bb350826aa58e7e5a2702b2618b658541b25cc9
+## Notes
+- Work in progress...
+- Still working in a better ML model to get better results in the colors of the trafficlight.
+- Still working in getting work the algorith with just one camera, the PiCamera.
+- We are still recolecting video data from infractions to reemplace critical parts of the algorithm with RLL the learn the events of cars.
+
+## Copyright
+See [LICENSE](LICENSE) for details.
+
+Copyright (c) 2018 (http://deepmicrosystems.com/).
+
--- ownLibraries/shutterv11.py
+++ ownLibraries/shutterv11.py
@@ -17,8 +17,6 @@ import pandas as pd
 import sqlite3
 import multiprocessing
 
-from .observerpicamera import Observer
-
 
 
 class Shutter(multiprocessing.Process):
@@ -83,7 +81,7 @@ class Shutter(multiprocessing.Process):
 		self.circular_buff_shooter 	= collections.deque(maxlen=12)
 
 		# Queues for comunicate the information between the Observer and Shutter
-		self.input_q  = multiprocessing.Queue()
+		self.input_q  	= multiprocessing.Queue()
 
 		self.observador = Observer(self.input_q)
 		self.observador.start()
@@ -129,15 +127,248 @@ class Shutter(multiprocessing.Process):
 
 		print('END OF RUN IN SHUTTER????')
 		print('Finished...')
+
+
+
+
+class Observer(multiprocessing.Process):
+
+	nombreCarpeta 		= datetime.datetime.now().strftime('%Y-%m-%d')+'_reporte'
+	directorioDeReporte = os.getenv('HOME')+'/' + nombreCarpeta
+	directorioDeNumpy 	= os.getenv('HOME')+'/trafficFlow/prototipo/installationFiles/'
+	directorioROOT 		= os.getenv('HOME')
+	date_hour_string 	= datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S:%f')
+	path_to_logo 		= os.getenv('HOME')+'/'+ 'trafficFlow' +'/' +'prototipo/'+ 'watermark'+ '/dems.png'
+
+	path_to_run_camera 		= os.getenv('HOME')+'/'+ 'WORKDIR' + '/' + 'run_camera.npy'
+	path_to_tasks_deque 	= os.getenv('HOME')+'/'+ 'WORKDIR' + '/' + 'tasks_deque.npy'
+
+
+	def __init__(self, input_q):
+		super(Observer, self).__init__()
+		# Dir where to save images
+
+		self.directorioDeGuardadoGeneral = Observer.directorioDeReporte
+		self.root 						 = Observer.directorioROOT
+		self.fechaInfraccion 			 = str
+		self.saveDir 					 = str
+		self.frame_number 				 = 0
+
+		self.run_camera 				 = False
+
+
+		self.frame_marcado 				 = str
+		self.folder 					 = str
+		folder_WORK 					 = 'WORKDIR'
+		self.saveDirWORK 				 = self.root + "/" + folder_WORK
+		self.save_with_name				 = str  							# aux name for images
+
+		# Fro db
+		self.date_for_db		 		= str(datetime.datetime.now().strftime('%Y-%m-%d'))
+
+
+		# Create circular buff deque of len 6
+		self.circular_buff 				 = collections.deque(maxlen=12)
+
+		# load queues from exterior world
+		self.input_q = input_q
+
+		# create watermarker class
+		self.watermarker = WaterMarker(Observer.path_to_logo)
+
+
+		print('Sucessfully started Observer CLASS!!!')
+
+
+	def leer_DB(self):
+		# Read old metadata
+		self.date_for_db = 	str(datetime.datetime.now().strftime('%Y-%m-%d'))
+
+		path_to_metadata = 	os.getenv('HOME')+'/'+ 'WORKDIR' + '/' + 'shooter_database_{}_cache.db'.format(self.date_for_db)
+		
+		# Init DB
+		conn 			 = 	sqlite3.connect(path_to_metadata,timeout=1)
+		c 				 =  conn.cursor()
+		#c.execute("SELECT * FROM stufftoPlot WHERE value=3 AND keyword='Python'")
+		#c.execute("SELECT keyword,unix,value FROM stufftoPlot WHERE unix >1515634491")
+		c.execute("SELECT * FROM shooter_table WHERE Status = 'OPEN'")
+		data = c.fetchall()
+
+		homework = []
+		for row in data:
+			metadata = data
+			homework.append(metadata)
+
+		c.execute("UPDATE shooter_table SET Status = ? WHERE Status = ?", ('CLOSED','OPEN'))
+		conn.commit()
+
+		c.close()
+		conn.close()
+
+		return homework
+
+	def move_captures(self, index_real):
+		self.frame_marcado = index_real
+		if self.frame_marcado != None:
+			# Once the while is finish move the files to his folders.
+			self.move_relevant_files(self.frame_marcado)
+		else:
+			pass
+
+
+	def encenderCamaraEnSubDirectorio(self, fecha, folder):
+
+		# Load variables
+		self.fechaInfraccion 	= fecha
+		self.folder 			= folder
+		self.saveDir 			= Observer.directorioDeReporte +"/" + str(self.folder)
+
+
+		# Check if exist folder where to save images
+		existe = os.path.exists(self.saveDir)
+		if not os.path.exists(self.saveDir):
+			os.makedirs(self.saveDir)
+			#os.mkdir(self.saveDir)
+
+		# Create aux folder to work in
+		if not os.path.exists(self.saveDirWORK):
+			os.makedirs(self.saveDirWORK) 
+
+		#print('Cree WORKDIR para trabajar el buffer de Forma Exitosa en ' + self.saveDirWORK + ' para: '+ self.saveDir)
+
+		self.save_with_name = self.saveDir + "/{}".format(self.fechaInfraccion)
+
+	def move_relevant_files(self, frame_marcado):
+		marcados_list  = []
+		print('frame_marcado is', frame_marcado)
+		print('self. circular_buff is', self.circular_buff)
+		for i, image_route in enumerate(self.circular_buff):
+			image_route_splited = image_route.split('i')
+
+			if frame_marcado in image_route_splited:
+				marcados_list.append(i)
+			# If index is not in list, chose his value - 1
+			else:
+				frame_marcado_as_int = int(frame_marcado) - 1
+				frame_marcado_as_str = str(frame_marcado_as_int)
+
+				if frame_marcado_as_str in image_route_splited:
+					marcados_list.append(i)
+				else:
+					pass
+
+		print('LIST OF AMRCADOS IS', marcados_list)
+		if len(marcados_list) != 0:
+			for marcado_frame in marcados_list:
+
+				#indice = self.circular_buff.index(marcado_frame)
+				indice = marcado_frame
+
+				src_0 = self.circular_buff[indice] 
+					
+				dst_0 = self.save_with_name + '_0.jpg'
+
+				try:
+					src_one = self.circular_buff[indice+1]
+					dst_one = self.save_with_name + '_1.jpg'
+				except:
+					src_one = self.circular_buff[indice-2]
+					dst_one = self.save_with_name + '_1.jpg'
+
+				try:
+					src_two = self.circular_buff[indice-2]
+					dst_two = self.save_with_name + '_2.jpg' # -1
+				except Exception as e:
+					print('src two cant move by this:', e)
+					src_two = self.circular_buff[indice]
+					dst_two = self.save_with_name + '_2.jpg' # -1
+				self.copiar_las_imagenes(src_0,dst_0,src_one, dst_one, src_two, dst_two)
+
+			else:
+				pass
+			self.frame_marcado = None
+
+	@staticmethod
+	def copiar_las_imagenes(src_0, dst_0, src_one, dst_one, src_two, dst_two):
+		try:
+			#print('copying from:', src_0, 'to:', dst_0)
+			shutil.copy(src_0, dst_0)
+		except Exception as e:
+			#print('DELETION WARNING for {}, delering source {}'.format(dst_0, src_0))
+			print('OR:', e)
+
+		try:
+			#print('copying from:', src_one, 'to:', dst_one)
+			shutil.copy(src_one, dst_one)
+		except Exception as e:
+			#print('DELETION WARNING for {}, delering source {}'.format(dst_one, src_one))
+			print('OR:', e)
+
+		try:
+			shutil.copy(src_two, dst_two)
+		except Exception as e:
+			#print('DELETION WARNING for {}, delering source {}'.format(dst_two, src_two))
+			print('OR:', e)
+		print('Capturado posible infractor!')
+
+
+	def run(self):
+		run_camera = np.load(Observer.path_to_run_camera)
+		while run_camera == 1:
+			try:
+				folders_queue 		= np.load(Observer.path_to_tasks_deque)
+			except:
+				print('Bussy np.load(), passing...')
+				queu = collections.deque(maxlen=2)
+				np.save(Observer.path_to_tasks_deque, queu)
+
+			try:
+				path_image_workdir = self.input_q.get(timeout=5)
+				self.circular_buff.appendleft(path_image_workdir)
+
+				if  len(folders_queue) != 0:
+					print('Iam into the tasksss!!!, tasks are', len(folders_queue))
+					print('TASKs are,', folders_queue)
+					homework = self.leer_DB()
+					if len(homework) > 0: 
+						for work in homework:
+							timestamp 	= work[0][0]
+							date   		= work[0][1]
+							folder 		= work[0][1]
+							index_real  = work[0][2]
+
+						saveDir = Observer.directorioDeReporte + '/' + folder
+
+						timestamp = timestamp#+' index:'+ index_real
+						self.encenderCamaraEnSubDirectorio(date, folder)
+						self.move_captures(index_real)
+						#self.watermarker.put_watermark(saveDir, timestamp)
+					# return an empnty task basket
+					queu = collections.deque(maxlen=2)
+					np.save(Observer.path_to_tasks_deque, queu)
+			except:
+				pass
+			# return state of while loop camera
+			try:
+				run_camera = np.load(Observer.path_to_run_camera)
+			except:
+				#q = collections.deque(maxlen=2)
+				#np.save(Observer.path_image_workdir,q)
+				print('Cant Load run_camera state, turning off the OBSERVER')
+				run_camera = 0
+
+
+		print('EXIRING FROM OBSERVER....')
+
+
+
+
 if __name__ == '__main__':
 	shutt = Shutter()
 	shutt.start()
 	counter = 0
 	while True:
-
 		counter +=1 
-		print(counter)
-
 		if counter == 10:
 			print(shutt.circular_buff_shooter)
 		time.sleep(1)
